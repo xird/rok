35eac91bad30af0acd7bd79eeba4c19c4253f2da
Erno Kaikkonen <erno@ampli.fi>
2016-02-16 21:39:14 +1030

    Show bigger cards when hovering

--- 2016/public/ROKClientGame.js
+++ 2016/public/ROKClientGame.js
@@ -349,37 +349,39 @@ ROKGame.prototype.initClient = function() {
   });
   
   // Show bigger cards when hovering on them.
-/*  $('#game').on({
+  $('#game').on({
     mouseover: function () {
       var div = $(this);
       var big_w = 200;
       var big_h = 156;
+      div.data('width', div.css('width'));
+      div.data('height', div.css('height'));
+
       div.css("width", big_w + "px");
       div.css("height", big_h + "px");
 
       var img = div.find('img');
-      img.data('width', img.attr('width'));
       img.attr('width', big_w);
-      img.data('height', img.attr('height'));
       img.attr('height', big_h);
 
       $(this).css('z-index', 99);
     },
     mouseout: function () {
       var div = $(this);
-      var small_w = 117;
-      var small_h = 91;
+      var small_w = $(this).data('width');
+      var small_h = $(this).data('height');
+
       div.css("width", small_w + "px");
       div.css("height", small_h + "px");
 
       var img = $(this).find('img');
-      img.attr('width', img.data('width'));
-      img.attr('height', img.data('height'));
+      img.attr('width', small_w);
+      img.attr('height', small_h);
 
       $(this).css('z-index', 0);
     }
   }, ".card, .monster_cards_owned");
-  */
+
   
   // Leave game.
   $('#game').on("click", "#leave_game_button", function clickLeaveGame(){
