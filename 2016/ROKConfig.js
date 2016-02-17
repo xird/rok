82cec4cc17db9fbabfa4aef03a45d1714c58249f
Erno Kaikkonen <erno@ampli.fi>
2016-02-17 20:01:51 +1030

    removed copypasted comment block

--- 2016/ROKConfig.js
+++ 2016/ROKConfig.js
@@ -1,12 +1,3 @@
-/**
- * The server side version of the Game class, which inherits the base Game
- * class.
- *
- * TODO: Add log note about staying in Kyoto
- * TODO: Allow abandoning game when the other players have gone
- * TODO: Don't allow buying if there isn't enough money
- */
-
 ROKConfig = {
   'dev_override' : true
 }
